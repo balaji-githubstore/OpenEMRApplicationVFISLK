c56cfc07cac49aa9779fa63d3217886a27abd6b0
balaji-githubstore <balaji0017@gmail.com>
2021-07-26 13:30:15 +0530

    browser config

--- src/test/java/com/vfislk/openemrtest/LoginTest.java
+++ src/test/java/com/vfislk/openemrtest/LoginTest.java
@@ -5,6 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -19,8 +21,24 @@ public class LoginTest {
 	@BeforeMethod
 	public void setUp()
 	{
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
-		driver=new ChromeDriver();
+		String browser="ie";
+		
+		switch (browser.toLowerCase()) {
+		case "ff":	
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", "src/test/resources/driver/geckodriver.exe");
+			driver=new FirefoxDriver();
+			break;
+		case "ie":
+			System.setProperty("webdriver.ie.driver", "src/test/resources/driver/IEDriverServer.exe");
+			driver=new InternetExplorerDriver();
+			break;
+		default:
+			System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
+			driver=new ChromeDriver();
+			break;
+		}
+		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://demo.openemr.io/a/openemr/index.php");
@@ -56,7 +74,7 @@ public class LoginTest {
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 		
 		WebDriverWait wait=new WebDriverWait(driver, 50);
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Calendar']")));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Calendar']"))).click();
 		
 		String actualValue=driver.getTitle();	
 		Assert.assertEquals(actualValue,"OpenEMR");		
