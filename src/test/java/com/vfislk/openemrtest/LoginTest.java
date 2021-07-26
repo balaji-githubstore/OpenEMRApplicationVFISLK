28227bce34f7fa5ced2446a58d62106a0bf582de
balaji-githubstore <balaji0017@gmail.com>
2021-07-26 12:32:50 +0530

    @Before/AfterMethod

--- src/test/java/com/vfislk/openemrtest/LoginTest.java
+++ src/test/java/com/vfislk/openemrtest/LoginTest.java
@@ -9,19 +9,46 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
+	private WebDriver driver;
 	
-	@Test(description = "Valid Credential Test")
-	public void validCredentialTest()
+	@BeforeMethod
+	public void setUp()
 	{
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://demo.openemr.io/a/openemr/index.php");
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+	@Test
+	public void invalidCredentialTest()
+	{		
+		driver.findElement(By.id("authUser")).sendKeys("admin123");
+		driver.findElement(By.id("clearPass")).sendKeys("pass");	
+		Select selectLanguage=new Select(driver.findElement(By.name("languageChoice")));
+		selectLanguage.selectByVisibleText("English (Indian)");		
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+			
+		String actualValue= driver.findElement(By.xpath("//div[contains(text(),'Invalid')]")).getText().trim();
 		
+		Assert.assertEquals(actualValue, "Invalid username or password");
+	}
+	
+	@Test(description = "Valid Credential Test")
+	public void validCredentialTest()
+	{		
 		driver.findElement(By.id("authUser")).sendKeys("admin");
 		driver.findElement(By.id("clearPass")).sendKeys("pass");	
 		Select selectLanguage=new Select(driver.findElement(By.name("languageChoice")));
@@ -33,8 +60,7 @@ public class LoginTest {
 		
 		String actualValue=driver.getTitle();	
 		Assert.assertEquals(actualValue,"OpenEMR");		
-		
-		driver.quit();
+			
 	}	
 	
 	
