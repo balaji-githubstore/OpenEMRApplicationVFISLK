370663397170d4f8c86d83eadaa9c543b5bbc220
balaji-githubstore <balaji0017@gmail.com>
2021-07-26 11:28:23 +0530

    day 1 framework - setup

---
+++ src/test/java/com/vfislk/openemrtest/LoginTest.java
@@ -0,0 +1,30 @@
+package com.vfislk.openemrtest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void validCredentialTest()
+	{
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://demo.openemr.io/b/openemr/interface/login/login.php?site=default");
+		
+		driver.findElement(By.id("authUser")).sendKeys("admin");
+		driver.findElement(By.id("clearPass")).sendKeys("pass");	
+		Select selectLanguage=new Select(driver.findElement(By.name("languageChoice")));
+		selectLanguage.selectByVisibleText("English (Indian)");		
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+	}	
+	
+}
