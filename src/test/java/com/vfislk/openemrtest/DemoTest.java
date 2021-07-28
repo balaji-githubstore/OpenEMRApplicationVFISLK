2fb6afa293afc1d169aaac78ab7d099c260cf7a6
balaji-githubstore <balaji0017@gmail.com>
2021-07-28 11:20:38 +0530

    excel read  with row/col index- /com/vfislk/openemrtest/DemoTest.java

--- src/test/java/com/vfislk/openemrtest/DemoTest.java
+++ src/test/java/com/vfislk/openemrtest/DemoTest.java
@@ -3,24 +3,36 @@ package com.vfislk.openemrtest;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
+import org.testng.annotations.Test;
 
 
 public class DemoTest {
-
-	public static void main(String[] args) throws IOException  {
+	
+	@Test
+	public void excelRead() throws IOException  {
 	
 		FileInputStream file=new FileInputStream("src/test/resources/testdata/OpenEMRData.xlsx"); //location - read
 		
-		XSSFWorkbook book=new XSSFWorkbook(file);
+		XSSFWorkbook book=new XSSFWorkbook(file); //open
 		
+		XSSFSheet sheet=  book.getSheet("invalidCredentialTest"); //sheet
 		
+		XSSFRow row=sheet.getRow(1); //row
 		
-
+		XSSFCell cell= row.getCell(0);//cell
 		
+		DataFormatter format=new DataFormatter();		
+		String cellValue = format.formatCellValue(cell);
 		
+		System.out.println(cellValue);
 		
+		book.close();
+		file.close();
 	}
 
 }
